4e0fc5a573192aa1bddcda81bae34f771adcfcdc
LeeSohyeon <teabag759@gmail.com>
2023-11-26 23:15:59 +0900

    [수정해야 할 것] 텍스트의 위치 정중앙으로 옮기기 또는 텍스트의 범위에 맞춰 UITextView 사이즈 변경하는 방법 알아보기

--- ToYou/ViewController.swift
+++ ToYou/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var UITextView: UITextView!
+    
+    @IBAction func clickButtonPressed(_ sender: UIButton) {
+        UITextView.textAlignment = .center
+        let toYou = ["When you have faults, do not fear to abandon them - Confucius",
+                     "Age is no guarantee of maturity - Lawana Blackwell",
+                     "You will face many defeats in life, but never let yourself be defeated - Maya Angelou",
+                     "Life is either a daring adventure or nothing at all - Helen Keller",
+                     "The goal of life is living in agreement with nature - Zeno",
+                     "Only I can change me life, no one can do it for me - Carol Burnett"]
+        
+        UITextView.text = toYou.randomElement()
     }
-
-
+    
 }
 
